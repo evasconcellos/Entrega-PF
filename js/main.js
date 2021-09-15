d54646118e69fd5482196d613e57288265e6136f
Erik Vasconcellos <erikvasconcellos@gmail.com>
2021-09-15 00:17:59 -0300

    Cambios con Jquery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 
 let carrito = [];
-if(localStorage.carrito != null){
+
+
+function storageCarrito () {
+  if(localStorage.carrito != null){
   carrito = JSON.parse(localStorage.carrito);
-  document.getElementsByClassName("contadorCarrito").innerHTML = carrito.length
+  $("#contadorCarrito").html(carrito.length);
 }
+}
+storageCarrito()
 
 
 class Productos {
@@ -17,54 +22,61 @@ class Productos {
 }
 
 
-const art1 = (new Productos(`Pack de 2 velas`, 300, 100, "IMG/Productos/velas2.jpg", `pack de 2 velas de diversos colores con el logo de Creo` ));
-const art2 = (new Productos(`Portavelas`, 500, 120, "IMG/Productos/portavelas4.jpg", `porta velas de diversos colores`));
-const art3 = (new Productos(`Kit cristales y loción`, 1250, 100, "IMG/Productos/kit1.jpg", `kit alquimizante (botella de loción, bolsa con logo de creo y cristales de diversos colores`));
-const art4 = (new Productos(`Kit Sahumado`, 550, 130, "IMG/Productos/kitsahumador.jpg", `kit sahumador, con hiervas secas y carbones para quemar`));
-const art5 = (new Productos(`Kit Sahumado 2`, 750, 80, "IMG/Productos/kitsahumador2.jpg", `kit sahumador, sahumador de barro con hiervas secas y carbones dentro`));
-const art6 = (new Productos(`Sahumador de barro`, 450, 130, "IMG/Productos/sahumadorbarro.jpg", `sahumador de barro con tapa con orificios y cadena para colgar`));
-const art7 = (new Productos(`Caja de sahumerios`, 350, 130, "IMG/Productos/sahumerios2.jpg", `Caja de sahumerios de la marca Nag Champa`));
-const art8 = (new Productos(`Loción alquimizada`, 350, 80, "IMG/Productos/locion.jpg", `botella de spray de 50ml con locion alquimizante`));
-const art9 = (new Productos(`Kit cristales 7 chakras`, 500, 100, "IMG/Productos/kitpiedraschakras.jpg", `7 cristales con los colores de los chakras`));
-const art10 = (new Productos(`Totebag`, 1800, 120, "IMG/Productos/totebag.jpg", `Bolsas de tela de color negro y beige con el logo de Creo estampado`));
-const art11 = (new Productos(`Caja Creo`, 3000, 130, "IMG/Productos/cajacreo2.jpg",`Interior de la caja Creo que contiene, velas, sahumerios, un porta vela, una loción alquimizante, y 7 cristales con los colores de los chakras`));
+const articulo1 = (new Productos(`Pack de 2 velas`, 300, 100, "IMG/Productos/velas2.jpg", `pack de 2 velas de diversos colores con el logo de Creo` ));
+const articulo2 = (new Productos(`Portavelas`, 500, 120, "IMG/Productos/portavelas4.jpg", `porta velas de diversos colores`));
+const articulo3 = (new Productos(`Kit cristales y loción`, 1250, 100, "IMG/Productos/kit1.jpg", `kit alquimizante (botella de loción, bolsa con logo de creo y cristales de diversos colores`));
+const articulo4 = (new Productos(`Kit Sahumado`, 550, 130, "IMG/Productos/kitsahumador.jpg", `kit sahumador, con hiervas secas y carbones para quemar`));
+const articulo5 = (new Productos(`Kit Sahumado 2`, 750, 80, "IMG/Productos/kitsahumador2.jpg", `kit sahumador, sahumador de barro con hiervas secas y carbones dentro`));
+const articulo6 = (new Productos(`Sahumador de barro`, 450, 130, "IMG/Productos/sahumadorbarro.jpg", `sahumador de barro con tapa con orificios y cadena para colgar`));
+const articulo7 = (new Productos(`Caja de sahumerios`, 350, 130, "IMG/Productos/sahumerios2.jpg", `Caja de sahumerios de la marca Nag Champa`));
+const articulo8 = (new Productos(`Loción alquimizada`, 350, 80, "IMG/Productos/locion.jpg", `botella de spray de 50ml con locion alquimizante`));
+const articulo9 = (new Productos(`Kit cristales 7 chakras`, 500, 100, "IMG/Productos/kitpiedraschakras.jpg", `7 cristales con los colores de los chakras`));
+const articulo10 = (new Productos(`Totebag`, 1800, 120, "IMG/Productos/totebag.jpg", `Bolsas de tela de color negro y beige con el logo de Creo estampado`));
+const articulo11 = (new Productos(`Caja Creo`, 3000, 130, "IMG/Productos/cajacreo2.jpg",`Interior de la caja Creo que contiene, velas, sahumerios, un porta vela, una loción alquimizante, y 7 cristales con los colores de los chakras`));
 
 const productos = [
-  art1, art2, art3, art4, art5, art6, art7, art8, art9, art10, art11
+  articulo1, articulo2, articulo3, articulo4, articulo5, articulo6, articulo7, articulo8, articulo9, articulo10, articulo11
 ];
 
 
+
 let mostrar = ""
+
+function mostrarProductos() { 
 productos.forEach (producto => {
 mostrar += ` 
 <div class="product-container">
   <h3 class="text-center">${producto.nombre}</h3>
   <img src="${producto.img}" alt="${producto.alt}"></>
   <h2>${producto.precio}</h2>
-  <button class="button-add" onclick="add('${producto.nombre}')">Agregar al carrito</button>
+  <button class="button-add" onclick="addCarrito('${producto.nombre}')">Agregar al carrito</button>
 </div>
 `
 })
 
-document.getElementById("page-content").innerHTML = mostrar
+$("#page-content").html(mostrar) 
+
+}
+mostrarProductos ()
 
 
-function add(nombre) {
-  const buscandoArt = productos.find(productos => productos.nombre === nombre)
-  if(buscandoArt != undefined){
-  carrito.push(buscandoArt);         
+function addCarrito(nombre) {
+  const buscandoArticulo = productos.find(productos => productos.nombre === nombre)
+  if(buscandoArticulo != undefined){
+  carrito.push(buscandoArticulo);         
   } else{
       alert("No fue posible agregar el producto");
   }
-  document.getElementById("contadorCarrito").innerHTML = carrito.length; 
-  localStorage.carrito = JSON.stringify(carrito)
+  $("#contadorCarrito").html(carrito.length); 
+
+  localStorage.carrito = JSON.stringify(carrito);
   
 }
 
 
 // Esto queda horrible pero la idea era ir probando cómo hacerlo.
 
-function pagando(){ 
+function pagandoCompra(){ 
   let alCarrito = ``;
   carrito.forEach((e) => {
       alCarrito += `
@@ -75,5 +87,6 @@ function pagando(){
       <p>$ ${e.precio}</p>
       </div> `
   })
-  document.getElementById("contenedorDePago").innerHTML = alCarrito;
+  $("#contenedorDePago").html(alCarrito);
   }
+
